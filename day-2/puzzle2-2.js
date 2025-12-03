c0deeb155c72b63d3446e114c0e1db36c357307c
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-12-03 20:47:03 +0000

    Comment code

--- day-2/puzzle2-2.js
+++ day-2/puzzle2-2.js
@@ -9,15 +9,19 @@ let runningTotal = 0;
 function checkValue(value) {
   const stringValue = value.toString();
   const length = stringValue.length;
+  //create range of numbers up to length of digit
   const lengthRange = createRange(1, length);
 
   for (const step of lengthRange) {
+    //check if digit can be broken up into 'step' chunks, with at least 2 chunks e.g 123456 can be broken up into chunks of 1, 2 and 3
     if (length % step === 0 && length / step > 1) {
       const slices = [];
+      //chunk it
       for (let start = 0; start < length; start += step) {
         slices.push(value.toString().slice(start, start + step));
       }
 
+      //check if the chunks match
       if (slices.every((slice) => slice === slices[0])) {
         return (runningTotal += value);
       }

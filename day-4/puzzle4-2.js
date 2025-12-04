d311de14762235b827faccff678200dde6a741ab
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-12-04 07:55:07 +0000

    Puzzle 4-2 (took 2 mins what)

--- day-4/puzzle4-2.js
+++ day-4/puzzle4-2.js
@@ -2,3 +2,42 @@ import fs from "fs";
 
 const input = fs.readFileSync("./puzzle4-input.txt", "utf-8");
 const sampleInput = fs.readFileSync("./puzzle4-sampleInput.txt", "utf-8");
+
+const shelves = input.split("\n").map((shelf) => shelf.split(""));
+const maxSurroundingRolls = 3;
+let totalRolls = 0;
+let prevTotalRolls;
+
+while (totalRolls !== prevTotalRolls) {
+  prevTotalRolls = totalRolls;
+  for (let i = 0; i < shelves.length; i++) {
+    for (let j = 0; j < shelves[i].length; j++) {
+      if (shelves[i][j] === "@") {
+        checkSurroundings(j, i);
+      }
+    }
+  }
+}
+
+function checkSurroundings(x, y) {
+  let count = 0;
+  for (let ty = -1; ty <= 1; ty++) {
+    if (y + ty >= 0 && y + ty < shelves.length) {
+      for (let tx = -1; tx <= 1; tx++) {
+        if (x + tx >= 0 && x + tx < shelves[y].length) {
+          if (!(ty === 0 && tx === 0)) {
+            if (shelves[y + ty][x + tx] === "@") {
+              count++;
+            }
+          }
+        }
+      }
+    }
+  }
+  if (count <= maxSurroundingRolls) {
+    totalRolls++;
+    shelves[y][x] = ".";
+  }
+}
+
+console.log(totalRolls);

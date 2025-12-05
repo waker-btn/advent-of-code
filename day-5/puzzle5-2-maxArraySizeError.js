c85bc65072f61e157b8d56d90620732ea45df6e3
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-12-05 09:54:48 +0000

    Puzzle 5-2

---
+++ day-5/puzzle5-2-maxArraySizeError.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./puzzle5-input.txt", "utf-8");
+const sampleInput = fs.readFileSync("./puzzle5-sampleInput.txt", "utf-8");
+
+const prepareFile = input.split("\n\n");
+const freshRanges = prepareFile[0].split("\n");
+const allFreshIds = new Set();
+
+function createRange(start, end) {
+  return Array.from(
+    {
+      length: Math.ceil(parseInt(end) - parseInt(start) + 1),
+    },
+    (unused, i) => parseInt(start) + i
+  );
+}
+
+for (const range of freshRanges) {
+  const startEnd = range.split("-");
+  console.log(startEnd);
+  const rangeArray = createRange(startEnd[0], startEnd[1]);
+  rangeArray.forEach((id) => allFreshIds.add(id));
+}
+
+console.log(allFreshIds.size);
--- day-5/puzzle5-2.js
+++ day-5/puzzle5-2.js
@@ -1,25 +1,65 @@
+//This can 100% be simplified further, but it works for now, and my brain hurts
+
 import fs from "fs";
 
 const input = fs.readFileSync("./puzzle5-input.txt", "utf-8");
 const sampleInput = fs.readFileSync("./puzzle5-sampleInput.txt", "utf-8");
 
 const prepareFile = input.split("\n\n");
-const freshRanges = prepareFile[0].split("\n");
-const allFreshIds = new Set();
+let freshRanges = prepareFile[0]
+  .split("\n")
+  .map((range) => range.split("-").map(Number));
+let prevSimplifiedRanges = [];
+let totalFreshRanges = 0;
 
-function createRange(start, end) {
-  return Array.from(
-    {
-      length: Math.ceil(parseInt(end) - parseInt(start) + 1),
-    },
-    (unused, i) => parseInt(start) + i
-  );
+function simplifyRanges() {
+  prevSimplifiedRanges = freshRanges.slice();
+  const newFreshRanges = [];
+  for (const range of freshRanges) {
+    let simplified = false;
+
+    for (let fresh of newFreshRanges) {
+      if (range[0] >= fresh[0] && range[1] <= fresh[1]) {
+        simplified = true;
+        break;
+      } else if (
+        range[0] <= fresh[0] &&
+        range[1] <= fresh[1] &&
+        range[1] >= fresh[0]
+      ) {
+        simplified = true;
+        fresh[0] = range[0];
+        break;
+      } else if (
+        range[0] >= fresh[0] &&
+        range[1] >= fresh[1] &&
+        range[0] <= fresh[1]
+      ) {
+        simplified = true;
+        fresh[1] = range[1];
+        break;
+      } else if (range[0] <= fresh[0] && range[1] >= fresh[1]) {
+        simplified = true;
+        fresh[0] = range[0];
+        fresh[1] = range[1];
+        break;
+      }
+    }
+
+    if (!simplified) {
+      newFreshRanges.push(range);
+    }
+  }
+  freshRanges = newFreshRanges;
 }
 
-for (const range of freshRanges) {
-  const startEnd = range.split("-");
-  const rangeArray = createRange(startEnd[0], startEnd[1]);
-  rangeArray.forEach((id) => allFreshIds.add(id));
+while (freshRanges.length !== prevSimplifiedRanges.length) {
+  simplifyRanges();
 }
 
-console.log(allFreshIds.size);
+console.log(freshRanges);
+freshRanges.forEach((range) => {
+  totalFreshRanges += range[1] - range[0] + 1;
+});
+
+console.log(totalFreshRanges);

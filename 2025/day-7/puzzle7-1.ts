cd347410265ed6dbc9abe4f69fd0d6d1e98d245f
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-12-07 08:44:23 +0000

    Add TS and 2025 Day 7
    Completed part 1, stuck for ideas on part 2

---
+++ 2025/day-7/puzzle7-1.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+
+const input = fs.readFileSync("./input.txt", "utf-8");
+const sampleInput = fs.readFileSync("./sampleInput.txt", "utf-8");
+
+const layout = input.split("\n").map((row) => row.split(""));
+const invert = layout[0].map((_, colIndex) =>
+  layout.map((row) => row[colIndex])
+);
+
+const start = [layout[0].findIndex((cell) => cell === "S"), 0];
+let totalSplit = 0;
+const visited = new Set<string>();
+
+function checkAhead(startPosition: number[]) {
+  const key = startPosition.join(",");
+  if (visited.has(key)) return;
+  visited.add(key);
+
+  console.log(`Checking ${startPosition}`);
+  const [y, x] = startPosition;
+  const path = invert[y].slice(x);
+
+  if (!path.length) {
+    return;
+  }
+
+  for (let cell = 0; cell < path.length; cell++) {
+    if (path[cell] === "^") {
+      totalSplit++;
+      invert[y][cell + x] = "X";
+      if (y - 1 >= 0) checkAhead([y - 1, cell + x]);
+      if (y + 1 < invert.length) checkAhead([y + 1, cell + x]);
+      break;
+    } else if (path[cell] === "X") {
+      if (y - 1 >= 0) checkAhead([y - 1, cell + x]);
+      if (y + 1 < invert.length) checkAhead([y + 1, cell + x]);
+      break;
+    }
+  }
+}
+
+checkAhead(start);
+console.log(totalSplit);
